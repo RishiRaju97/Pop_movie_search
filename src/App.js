8adf962287b1e956860cfc07b4973f5a4c1787bb
Rishi R <raju.s.rishi20@student.rau.ro>
2023-12-26 16:32:52 +0530

    Movie search project

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import searchIcon from './search.svg';
+import './App.css';
+import MovieCard from "./movieCard";
+
+
+// API key = 445f02ac
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=445f02ac';
+
+const App = () => {
+
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        setMovies(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('Superman');
+    }, []);
+
+    return (
+        <div className="app">
+            <h1>Poptime</h1>
+
+            <div className="search">
+                <input placeholder="Type a movie name" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value) } />
+                <img src={searchIcon} alt="search" onClick={() => searchMovies(searchTerm)} />
+            </div>
+
+            {
+                movies?.length > 0
+                    ? (
+                        <div className="container">
+                            {movies.map((movie)=>(
+                                 <MovieCard movie={movie} />
+                            ))}
+                        </div>
+                    ) : (
+                        <div className="empty">
+                            <h2>No movies found</h2>
+                        </div>
+                    )
+            }
+
+
+
+        </div>
+    );
+}
+
+export default App;
